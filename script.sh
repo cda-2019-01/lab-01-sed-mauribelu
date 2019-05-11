3ca73f249b1e485bb6dfbfd9ec56d9aba3ea6bbd
mauribelu <mauribelu@unal.edu.co>
2019-05-10 23:12:25 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 #Transformacion de fecha
-sed 's/\//-/g' data0.csv > data1.csv
+sed 's/\//-/g' data.csv > data1.csv
 sed 's/-\([0-9][0-9]\);/-20\1;/' data1.csv > data2.csv
 sed 's|\([0-9][0-9]\)-\([0-9][0-9]\)-\([0-9][0-9][0-9][0-9]\);|\3-\2-\1;|g' data2.csv > data3.csv
 #Reemplazo de las , decimales por punto
@@ -14,9 +14,9 @@ sed 's/a/A/g' data5.csv > data6.csv
 #reemplazo las c minuscula por C mayúscula
 sed 's/c/C/g' data6.csv > data7.csv
 #reemplazar campos nulos por /N
-sed -e 's:^,:/N,:g' -e 's:,,:,/N,:g' -e 's:,$:,/N:g' data7.csv > data8.csv
+sed -e 's:^,:\\N,:g' -e 's:,,:,\\N,:g' -e 's:,,:,\\N,:g' -e 's:,$:,\\N:g' data7.csv > data8.csv
 #generar los registros que no tienen registros nulos
-sed '\:/N:d' data6.csv > LabFinal1.csv
+sed '\:/N:d' data9.csv > LabFinal1.csv
 rm data*
 
 cat LabFinal1.csv
